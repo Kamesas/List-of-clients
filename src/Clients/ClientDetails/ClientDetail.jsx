74cd5502e261b6c6865fbf5f9c4eec8049ec800c
Kamesas <semakaleksandr2014@gmail.com>
2018-12-15 10:34:39 +0200

    added styles for ClientDetail component

--- src/Clients/ClientDetails/ClientDetail.jsx
+++ src/Clients/ClientDetails/ClientDetail.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { List, Header, Grid, Icon, Image } from "semantic-ui-react";
 
 const ClientDetail = () => {
-  return <div>ClientDetail</div>;
+  return (
+    <Grid>
+      <Grid.Column width={4}>
+        <Image src="/images/avatar/small/helen.jpg" />
+      </Grid.Column>
+      <Grid.Column width={12}>
+        <Header as="h1">Name SecondName</Header>
+        <List size={"big"}>
+          <List.Item>
+            <strong>Company: </strong> company
+          </List.Item>
+          <List.Item>
+            <strong>Profession: </strong> profession
+          </List.Item>
+        </List>
+        <List>
+          <List.Item>
+            <Icon name="envelope" />
+            email
+          </List.Item>
+          <List.Item>
+            <Icon name="phone" />
+            phone
+          </List.Item>
+        </List>
+
+        <List>
+          <List.Item>
+            <strong>Country: </strong>
+            country
+          </List.Item>
+          <List.Item>
+            <strong>City: </strong> city
+          </List.Item>
+          <List.Item>
+            <strong>Street: </strong> street
+          </List.Item>
+          <List.Item>
+            <strong>ZipCode: </strong> zipCode
+          </List.Item>
+        </List>
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default ClientDetail;
