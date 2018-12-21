7f70e08c52f66c9bbf1ddf99f3507a1be3e11fed
Alexandr Semak <semakaleksandr2014@gmail.com>
2018-12-21 12:18:51 +0200

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,12 +1,7 @@
 import * as firebase from "firebase";
 
 const FirebaseConfig = {
-  apiKey: "AIzaSyCDF7P12-_oQkeAk71Sm_ro9nddyohU8QQ",
-  authDomain: "listofclients-a081f.firebaseapp.com",
-  databaseURL: "https://listofclients-a081f.firebaseio.com",
-  projectId: "listofclients-a081f",
-  storageBucket: "listofclients-a081f.appspot.com",
-  messagingSenderId: "1053310062783"
+ 
 };
 
 firebase.initializeApp(FirebaseConfig);
